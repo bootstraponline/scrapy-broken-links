94ee4c1eef29a0697e9d2466977dbf9d9dd4e749
bootstraponline <code@bootstraponline.com>
2014-08-27 13:37:52 -0400

    scrapy startproject broken_links

---
+++ broken_links/broken_links/settings.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+# Scrapy settings for broken_links project
+#
+# For simplicity, this file contains only the most important settings by
+# default. All the other settings are documented here:
+#
+#     http://doc.scrapy.org/en/latest/topics/settings.html
+#
+
+BOT_NAME = 'broken_links'
+
+SPIDER_MODULES = ['broken_links.spiders']
+NEWSPIDER_MODULE = 'broken_links.spiders'
+
+# Crawl responsibly by identifying yourself (and your website) on the user-agent
+#USER_AGENT = 'broken_links (+http://www.yourdomain.com)'
