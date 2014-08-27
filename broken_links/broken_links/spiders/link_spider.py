1e8592de6a99e6fdd02afddafcde34623c30b6f6
bootstraponline <code@bootstraponline.com>
2014-08-27 16:30:56 -0400

    Use LinkExtractor as suggested by Andres

--- broken_links/broken_links/spiders/link_spider.py
+++ broken_links/broken_links/spiders/link_spider.py
@@ -1,28 +1,40 @@
 # -*- coding: utf-8 -*-
 import scrapy
+from scrapy.contrib.linkextractors import LinkExtractor
+from scrapy.contrib.spiders import CrawlSpider, Rule
+
 from broken_links.items import BrokenLinksItem
-from urlparse import urlparse
 
 # scrapy runspider link_spider.py
 #
 # run from the project directory (broken_links/broken_links$ )
+# note that this will append to the items.json file if it exists instead of overriding.
+#
 # scrapy crawl link_spider -o items.json
-class LinkSpiderSpider(scrapy.Spider):
+class LinkSpiderSpider(CrawlSpider):
     # todo: configure download delay
     # http://doc.scrapy.org/en/latest/topics/settings.html#std:setting-DOWNLOAD_DELAY
     name = "link_spider"
-    allowed_domains = ["bootstraponline.github.io", "www.github.com"]
+    target_domain = "bootstraponline.github.io"
+    allowed_domains = [target_domain, "www.github.com"]
     start_urls = (
         'http://bootstraponline.github.io/scrapy-broken-links/',
     )
 
-    def parse(self, response):
-        item = BrokenLinksItem()
-        item['url'] = response.url
-        item['status'] = response.status
-        item['parent'] = response.request.url
-        # from http://doc.scrapy.org/en/latest/topics/spiders.html#topics-spiders
-        for url in response.xpath('//a/@href').extract():
-            # ensure url has a valid scheme before attempting to crawl it.
-            if urlparse(url).scheme != '':
-                yield scrapy.Request(url, callback=self.parse)
+    # http://doc.scrapy.org/en/latest/topics/link-extractors.html#topics-link-extractors
+    # http://doc.scrapy.org/en/latest/topics/spiders.html#scrapy.contrib.spiders.Rule
+    rules = (
+        Rule(LinkExtractor(allow_domains=[target_domain], unique=True), callback='parse_item', follow=True),
+        Rule(LinkExtractor(deny_domains=[target_domain],  unique=True), callback='parse_item', follow=False),
+    )
+
+def parse_item(self, response):
+    items = []
+
+    item = BrokenLinksItem()
+    item['url'] = response.url
+    item['status'] = response.status
+    item['parent'] = response.request.url
+
+    items.append(item)
+    return items
